a132c66916d64e829646ddefc74a88cb82389a3d
Vanessa-Natalie <vanessa.cipta@binus.ac.id>
2025-09-07 15:33:22 +0700

    Add files via upload

---
+++ sesi1.js
@@ -0,0 +1,29 @@
+/*
+width SET TO 3
+length SET TO 5
+
+FUNCTION rectangleArea (width, length){
+    area SET TO = width * length
+    RETURN
+}
+
+areaResult SET TO = rectangleArea (width, length)
+PRINT areaResult
+
+
+
+
+width SET TO [UNDEFIENED]
+length SET TO [UNDEFINED]
+
+FUNCTION rectangleArea (width, length){
+    width SET TO [USER INPUT]
+    length SET TO [USER INPUT]
+
+    area SET TO = width * length
+    RETURN
+}
+
+areaResult SET TO = rectangleArea (width, length)
+PRINT areaResult
+*/
